3e705294793ae113d4df60df3cf5118b66a01b33
Patin Inkaew <earthpatin@gmail.com>
2024-02-04 01:45:02 +0100

    add parsing of expression

--- ROOTColorDB.py
+++ ROOTColorDB.py
@@ -1,4 +1,5 @@
 import pickle
+import re
 
 predefined_colors = {
     "kWhite" : 0,
@@ -21,5 +22,20 @@ predefined_colors = {
 with open("ROOTColorDB.p", "rb") as file:
     color_hex_dict = pickle.load(file)
 
-def get_hex_color(ROOT_color_idx):
+def get_ROOT_color_as_hex(ROOT_color_idx):
+    def convert_to_color_idx_int(color_idx):    
+        try:
+            color_idx = int(color_idx)
+        except:
+            if color_idx in predefined_colors:
+                color_idx = predefined_colors[color_idx]
+        return color_idx
+    
+    if isinstance(ROOT_color_idx, str):
+        if ROOT_color_idx in predefined_colors:
+            ROOT_color_idx = predefined_colors[ROOT_color_idx]
+        else: 
+            ROOT_color_idx = eval("".join([str(convert_to_color_idx_int(token)) if token != "+" or token != "-" else token 
+                                           for token in re.split(r"(\+|-)", ROOT_color_idx)]))
+    
     return color_hex_dict[ROOT_color_idx]
